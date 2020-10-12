b357fdaa4fd21c6d0839bb8a7c4c7c1c71d73cce
rechassagn1 <rechassagn1@ada.local.isima.fr>
2020-10-12 15:31:40 +0200

    n

---
+++ jacobi/jacobi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LEN 4
+
+float ** d(float **matrix, int n){
+  float **d = 
+}
+
+int main(){
+  float *b = malloc(LEN*sizeof(float));
+  float *solus = malloc(LEN*sizeof(float));
+  float ** matrix = NULL;
+  float **matrix = (float**) malloc(LEN*sizeof(float*));
+  for (i = 0; i < LEN; i++){
+    matrix[i] = (float*) malloc(LEN*sizeof(float));
+  }
+
+  return 0;
+}
