1f4cd6dbc0a642bc0caa8011ac5766d80c3064b8
drfailer <drfailer.42@gmail.com>
2020-10-15 19:12:34 +0200

    Fix cholesky and jacobi done

--- cholesky/cholesky.c
+++ cholesky/cholesky.c
@@ -3,8 +3,8 @@
 #include <math.h>
 #define LEN 3
 
-void make_R(float** matrix, float** r, int n){
-  int i, j, k; //loop var
+void make_R(float **matrix, float **r, int n) {
+  int i, j, k;   // loop var
   float sum = 0; // Used to calculate the sum of r_ki*r_kj
   float s;
   for (i = 0; i < n; i++) {
@@ -15,26 +15,25 @@ void make_R(float** matrix, float** r, int n){
     if (s <= 0) {
       printf("La matrice n'est pas définie positive\n");
       exit(EXIT_FAILURE);
-    }
-    else {
-      r[i][i] = sqrt(s);
-      for (j = (i+1); j < n; j++) {
-	sum = 0;
-	for (k = 0; k < i; k++) {
-	  sum += r[k][i] * r[k][j];
-	}
-	r[i][j] = (matrix[i][j] - sum)/r[i][i];
+    } else {
+      r[i][i] = sqrtf(s);
+      for (j = (i + 1); j < n; j++) {
+        sum = 0;
+        for (k = 0; k < i; k++) {
+          sum += r[k][i] * r[k][j];
+        }
+        r[i][j] = (matrix[i][j] - sum) / r[i][i];
       }
     }
   }
 }
 
 // transpose matrix
-void transpose(float **matrix, int n){
-  int i,j;
+void transpose(float **matrix, int n) {
+  int i, j;
   float tmp;
   for (i = 0; i < n; i++) {
-    for (j = (i+1); j < (n-1); j ++) {
+    for (j = (i + 1); j < n; j++) {
       tmp = matrix[i][j];
       matrix[i][j] = matrix[j][i];
       matrix[j][i] = tmp;
@@ -42,13 +41,13 @@ void transpose(float **matrix, int n){
   }
 }
 
-void solver_cholesky(float **matrix,float *solus_x, float *b, int n){
+void solver_cholesky(float **matrix, float *solus_x, float *b, int n) {
   int i, j;
-  float *solus_y = malloc(n*sizeof(float));
+  float *solus_y = malloc(n * sizeof(float));
   /*Creation of the matrix  R:*/
-  float **r = malloc(n*sizeof(float*));
-  for (i = 0; i < n; i++){
-    r[i] =  malloc(n*sizeof(float));
+  float **r = malloc(n * sizeof(float *));
+  for (i = 0; i < n; i++) {
+    r[i] = malloc(n * sizeof(float));
   }
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
@@ -63,15 +62,15 @@ void solver_cholesky(float **matrix,float *solus_x, float *b, int n){
     for (j = 0; j < i; j++) {
       b[i] -= solus_y[j] * r[i][j];
     }
-    solus_y[i] = b[i]/r[i][i];
+    solus_y[i] = b[i] / r[i][i];
   }
-  /*FIXME: Solving R * x = y*/
+  /*Solving R * x = y*/
   transpose(r, n);
-  for (i = (n-1); i >= 0; i--) {
-    for (j = (n-1); j > i; j--) {
+  for (i = (n - 1); i >= 0; i--) {
+    for (j = (n - 1); j > i; j--) {
       solus_y[i] -= solus_x[j] * r[i][j];
     }
-    solus_x[i] = solus_y[i]/r[i][i];
+    solus_x[i] = solus_y[i] / r[i][i];
   }
   /*FREE*/
   free(solus_y);
@@ -81,23 +80,32 @@ void solver_cholesky(float **matrix,float *solus_x, float *b, int n){
   free(r);
 }
 
-int main()
-{
+int main() {
   int i;
-  float *solus = malloc(LEN*sizeof(float));
-  float *b = malloc(LEN*sizeof(float));
-  // TODO: Init b properly
-  b[0] = 1; b[1] = 1; b[2] = 1; b[3] = 1;
+  float *solus = malloc(LEN * sizeof(float));
+  float *b = malloc(LEN * sizeof(float));
   // alloc table matrix
-  float **matrix = (float**) malloc(LEN*sizeof(float*));
-  for (i = 0; i < LEN; i++){
-    matrix[i] = (float*) malloc(LEN*sizeof(float));
+  float **matrix = (float **)malloc(LEN * sizeof(float *));
+  for (i = 0; i < LEN; i++) {
+    matrix[i] = (float *)malloc(LEN * sizeof(float));
   }
+  // TODO: Init b properly
+  b[0] = 1;
+  b[1] = 1;
+  b[2] = 1;
   // TODO: Init matrix properly
-  matrix[0][0] = 64; matrix[0][1] = 40; matrix[0][2] = 24;
-  matrix[1][0] = 40; matrix[1][1] = 29; matrix[1][2] = 17;
-  matrix[2][0] = 24; matrix[2][1] = 17; matrix[2][2] = 19;
+  matrix[0][0] = 64;
+  matrix[0][1] = 40;
+  matrix[0][2] = 24;
+  matrix[1][0] = 40;
+  matrix[1][1] = 29;
+  matrix[1][2] = 17;
+  matrix[2][0] = 24;
+  matrix[2][1] = 17;
+  matrix[2][2] = 19;
+
   solver_cholesky(matrix, solus, b, LEN);
+  // Print solutions
   for (i = 0; i < LEN; i++) {
     printf("%f\n", solus[i]);
   }
--- jacobi/jacobi.c
+++ jacobi/jacobi.c
@@ -1,90 +1,97 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#define LEN 3
+#define LEN 4
 #define SIGMA 0.0000001
+#define MAX 100
 
-void jacobi(float ** matrix, float * b, float * start_point, int n);
-
-
-/* float calc_error(float ** mat, float * x, float * b, int n){ */
-/*   int i, j, k; */
-/*   int *tmp = malloc(n*sizeof(float*)); */
-/*   float output = 0; */
-/*   for(i = 0; i < n; i++){ */
-/*     tmp[i] = 0; */
-/*     for(j = 0; j < n; j++){ */
-/*       tmp[i] += mat[i][j] * x[j]; */
-/*     } */
-/*   } */
-/*   for(i = 0; i < n; i++){ */
-/*     tmp[i] -= b[i]; */
-/*     output += tmp[i] * tmp[i]; */
-/*   } */
-/*   printf("%.3f sigma\n",sqrtf(output)); */
-/*   free(tmp); */
-/*   return sqrtf(output); */
-/* } */
-
-int test(float * solus,float * lautre, int n){
+void jacobi(float **matrix, float *b, float *solus_k, int n);
+// FIXME:
+int test(float *solus_k, float *solus_k1, int n) {
   int i;
-  float * tmp = malloc(n*sizeof(float));
-  float s_quadra;
-  for(i = 0; i < n; i++){
-	tmp[i]  = solus[i] - lautre[i];
-	s_quadra += tmp[i] * tmp[i];
+  float *tmp = malloc(n * sizeof(float));
+  float s_quadra = 0;
+  for (i = 0; i < n; i++) {
+    tmp[i] = solus_k1[i] - solus_k[i];
+    s_quadra += tmp[i] * tmp[i];
   }
-  return sqrtf(s_quadra) > SIGMA;
+  // FREE
+  free(tmp);
+  return (sqrtf(s_quadra) > SIGMA);
 }
 
-int main(){
+int main() {
   int i;
-  float *b = malloc(LEN*sizeof(float));
-  b[0] = 8;  b[1] = 3;  b[2] = 8;
-  
-  float *solus = malloc(LEN*sizeof(float));
-  solus[0] = 0;  solus[1] = 0;  solus[2] = 1;
-  
-  float **matrix = (float**) malloc(LEN*sizeof(float*));
-  for (i = 0; i < LEN; i++){
-    matrix[i] = (float*) malloc(LEN*sizeof(float));
-  }
-  matrix[0][0] = 4;   matrix[0][0] = 1;   matrix[0][0] = 0;
-  matrix[0][0] = -1;   matrix[0][0] = 3;   matrix[0][0] = 6;
-  matrix[0][0] = -2;   matrix[0][0] = -5;   matrix[0][0] = -2;
+  float *b = malloc(LEN * sizeof(float));
+  b[0] = 15;
+  b[1] = 15;
+  b[2] = 19;
+  b[3] = 11;
 
-  jacobi(matrix, b, solus,3);
+  float *solus = malloc(LEN * sizeof(float));
+  // Init solus
+  solus[0] = 1;
+  solus[1] = 1;
+  solus[2] = 1;
+  solus[3] = 1;
+
+  float **matrix = (float **)malloc(LEN * sizeof(float *));
+  for (i = 0; i < LEN; i++) {
+    matrix[i] = (float *)malloc(LEN * sizeof(float));
+  }
+  // Init matrix
+  matrix[0][0] = 4;
+  matrix[0][1] = 1;
+  matrix[0][2] = 1;
+  matrix[0][3] = 0;
+  matrix[1][0] = 1;
+  matrix[1][1] = 4;
+  matrix[1][2] = 0;
+  matrix[1][3] = 1;
+  matrix[2][0] = 1;
+  matrix[2][1] = 0;
+  matrix[2][2] = 4;
+  matrix[2][3] = 1;
+  matrix[3][0] = 0;
+  matrix[3][1] = 1;
+  matrix[3][2] = 1;
+  matrix[3][3] = 4;
+
+  jacobi(matrix, b, solus, LEN);
+  for (i = 0; i < LEN; i++) {
+    printf("%f\n", solus[i]);
+  }
+  // FREE
   free(solus);
   free(b);
-  for(i = 0; i<LEN; i++)
-    {
-      free(matrix[i]); 
-    }
+  for (i = 0; i < LEN; i++) {
+    free(matrix[i]);
+  }
   free(matrix);
   return 0;
 }
 
-void jacobi(float ** matrix, float * b, float * start_point, int n){
+void jacobi(float **matrix, float *b, float *solus_k, int n) {
   int i, j;
-  float * solus = malloc(n*sizeof(float*));
-  
-  while (!test(solus, start_point, n)){
-
-    for(i = 0; i < n; i++){
-      printf("%.1f, ",start_point[i]);
-      solus[i] = b[i];
-      for(j = 0; j < n; j++){
-	if (j != i)
-	  solus[i] -= matrix[i][j] * start_point[j];
+  int compt = 0;
+  int bool = 1;
+  float *solus_k1 = malloc(n * sizeof(float *));
+  while (bool &&compt < MAX) {
+    for (i = 0; i < n; i++) {
+      solus_k1[i] = b[i];
+      for (j = 0; j < n; j++) {
+        if (j != i)
+          solus_k1[i] -= matrix[i][j] * solus_k[j];
       }
-      solus[i] /= matrix[i][i];
+      solus_k1[i] /= matrix[i][i];
     }
-    for(i = 0; i < n; i++){     
-      start_point[i] = solus[i];
-      
-
+    bool = test(solus_k1, solus_k, n);
+    for (i = 0; i < n; i++) {
+      solus_k[i] = solus_k1[i]; // At the end of the execution, solus_k contain
+                                // the solutions
     }
-    printf("\n");
+    compt++;
   }
-  free(solus);
+  // FREE
+  free(solus_k1);
 }
