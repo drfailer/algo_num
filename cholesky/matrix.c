9065eb43e98cbdb963a6caf4f339b99c405533a5
drfailer <drfailer.42@gmail.com>
2020-10-18 11:40:33 +0200

    update output for cholesky and gauss

---
+++ cholesky/matrix.c
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#include <math.h>
+
+// return nul matrix nxn
+void mat_0(float **tab, int n){
+  int i, j;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      tab[i][j] = 0;
+    }
+  }
+}
+
+void bords_carre(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i == j)
+	tab[i-1][j-1] = 1;
+      else if (i == 1 || j == 1)
+	tab[i-1][j-1] = 2 << (n-j);
+      else
+	tab[i-1][j-1] = 0;
+    }
+  }
+}
+
+void ding_dong(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      tab[i-1][j-1] = 1 / 2*(n - i - j + 1.5);
+    }
+  }
+}
+
+void franc(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i >= j+2)
+	tab[i-1][j-1] = 0;
+      else	
+	tab[i-1][j-1] = (i < j)? i:j;
+    }
+  }
+}
+
+void hilbert_1(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      tab[i-1][j-1] = 1 / (i + j - 1);
+    }
+  }
+}
+
+void hilbert_2(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      tab[i-1][j-1] = 1 / (i + j + 1);
+    }
+  }
+}
+
+void kms(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      tab[i-1][j-1] = powf(0.5, i-j);
+    }
+  }
+}
+
+void lehmer(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i <= j)
+	tab[i-1][j-1] = i/j;
+      else
+	tab[i-1][j-1] = j/i;
+    }
+  }
+}
+
+void lotkin(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i == 1)
+	tab[i-1][j-1] = 1;
+      else
+	tab[i-1][j-1] = 1 / (i + j -1);
+    }
+  }
+}
+
+void moler(float **tab, int n){
+  int i, j;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= n; j++) {
+      if (i == j)
+	tab[i-1][j-1] = i;
+      else
+	tab[i-1][j-1] = (i < j)? (i-2):(j-2);
+    }
+  }
+}
+
+void mat(float **mat, int n, int type_mat){
+  switch (type_mat) {
+  case 0: mat_0(mat, n); break;
+  case 1: bords_carre(mat, n); break;
+  case 2: ding_dong(mat, n); break;
+  case 3: franc(mat, n); break;
+  case 4: hilbert_1(mat, n); break;
+  case 5: hilbert_2(mat, n); break;
+  case 6: kms(mat, n); break;
+  case 7: lehmer(mat, n); break;
+  case 8: lotkin(mat, n); break;
+  case 9: moler(mat, n); break;
+  default: break;
+  }
+}
+
+float **create_mat(int n){
+  int i;
+  float ** mat = NULL;
+  mat = (float**) malloc(n*sizeof(float*));
+  if (mat == NULL)
+    exit(EXIT_FAILURE);
+  for (i = 0; i < n; i++) {
+    mat[i] = (float*) malloc(n*sizeof(float));
+    if (mat[i] == NULL)
+      exit(EXIT_FAILURE);
+  }
+  return mat;
+}
---
+++ cholesky/matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATRIX
+#define MATRIX
+
+void mat_0(float **tab, int n);
+void bords_carre(float **tab, int n);
+void ding_dong(float **tab, int n);
+void franc(float **tab, int n);
+void hilbert_1(float **tab, int n);
+void hilbert_2(float **tab, int n);
+void kms(float **tab, int n);
+void lehmer(float **tab, int n);
+void lotkin(float **tab, int n);
+void moler(float **tab, int n);
+void mat(float **mat, int n, int type);
+float **create_mat(int n);
+
+#endif
